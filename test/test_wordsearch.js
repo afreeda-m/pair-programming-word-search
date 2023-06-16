900476013a230f1188b2fad10a66ea557a3920f9
Afreeda <amahesan@uoguelph.ca>
2023-06-16 15:35:05 -0400

    add more test cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,37 +1,43 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require("../wordsearch.js");
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
+    const result = wordSearch(
+      [
+        ["A", "W", "C", "F", "Q", "U", "A", "L"],
+        ["S", "E", "I", "N", "F", "E", "L", "D"],
+        ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+        ["H", "M", "J", "T", "E", "V", "R", "G"],
+        ["W", "H", "C", "S", "Y", "E", "R", "L"],
+        ["B", "F", "R", "E", "N", "E", "Y", "B"],
+        ["U", "B", "T", "W", "A", "P", "A", "I"],
+        ["O", "D", "C", "A", "K", "U", "A", "S"],
+        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+      ],
+      "FRANK"
+    );
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+  it("should return true if the word is present", function () {
+    const result = wordSearch(
+      [
+        ["A", "W", "C", "F", "Q", "U", "A", "L"],
+        ["S", "E", "I", "N", "F", "E", "L", "D"],
+        ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+        ["H", "M", "J", "T", "E", "V", "R", "G"],
+        ["W", "H", "C", "S", "Y", "E", "R", "L"],
+        ["B", "F", "R", "E", "N", "E", "Y", "B"],
+        ["U", "B", "T", "W", "A", "P", "A", "I"],
+        ["O", "D", "C", "A", "K", "U", "A", "S"],
+        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+      ],
+      "SEINFELD"
+    );
 
     assert.isTrue(result);
   });
@@ -51,7 +57,14 @@ describe("#wordSearch()", function() {
       ],
       "FRANK"
     );
+
     assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function () {
+    const result = wordSearch([], "FRANK");
+
+    assert.isFalse(result);
   });
 
   it("should return true if the word is present", function () {
@@ -73,7 +86,6 @@ describe("#wordSearch()", function() {
     assert.isTrue(result);
   });
 
-
   it("should return true if the word is present", function () {
     const result = wordSearch(
       [
@@ -91,5 +103,26 @@ describe("#wordSearch()", function() {
     );
 
     assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function () {
+    const result = wordSearch([], "");
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is not present", function () {
+    const result = wordSearch([
+      ["A", "W", "C", "F", "Q", "U", "A", "L"],
+      ["S", "E", "I", "N", "F", "E", "L", "D"],
+      ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+      ["H", "M", "J", "T", "F", "V", "R", "G"],
+      ["W", "H", "C", "S", "R", "E", "N", "L"],
+      ["B", "F", "R", "E", "A", "E", "O", "B"],
+      ["U", "B", "T", "W", "N", "P", "R", "I"],
+      ["O", "D", "C", "A", "K", "N", "A", "S"],
+      ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+    ]);
+
+    assert.isFalse(result);
   });
 });
